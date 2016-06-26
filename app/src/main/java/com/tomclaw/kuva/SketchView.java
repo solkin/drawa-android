483b425b544cd09527dd9de4b53f135937e17596
Igor Solkin <inbox@tomclaw.com>
2016-06-26 23:39:00 +0300

    Доработка инструментов

--- app/src/main/java/com/tomclaw/kuva/SketchView.java
+++ app/src/main/java/com/tomclaw/kuva/SketchView.java
@@ -14,36 +14,38 @@ import java.util.List;
  */
 public class SketchView extends View {
 
-    private float VELOCITY_FILTER_WEIGHT = 0.2f;
-
     private List<Bitmap> backup;
 
     private Paint paint;
 
     private float prevX, prevY;
     private Point prevPoint;
-    private float prevVelocity;
-    private int trackLength;
     private Path path;
     private Bitmap bitmap;
     private Canvas canvas;
     private Paint simplePaint;
 
-    private final int scaleFactor = 1;
+    private final float scaleFactor = 1.5f;
 
     private Rect src, dst;
 
-    private int radius;
+    private int alpha = 0x50;
+    private boolean isVarRadius = false;
+
+    private int baseRadius = 60;
 
     public SketchView(Context context, AttributeSet attrs) {
         super(context, attrs);
         backup = new ArrayList<>();
 
-        // initFluffy();
         initPencil();
+//        initBrush();
+//        initMarker();
+//        initFluffy();
+//        initEraser();
 
-        radius(80 / scaleFactor);
-        color(0xE6B82A);
+        radius(baseRadius / scaleFactor);
+        color(0xcd0219);
 
         path = new Path();
         simplePaint = new Paint();
@@ -52,10 +54,24 @@ public class SketchView extends View {
         simplePaint.setDither(true);
     }
 
-    private void initFluffy() {
+    private void initMarker() {
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.MITER);
+        paint.setStrokeCap(Paint.Cap.BUTT);
         paint.setPathEffect(new DashPathEffect(new float[]{2, 0}, 0));
+        alpha = 0x50;
+        isVarRadius = false;
+    }
+
+    private void initBrush() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        alpha = 0xff;
+        isVarRadius = true;
     }
 
     private void initPencil() {
@@ -64,6 +80,20 @@ public class SketchView extends View {
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStrokeCap(Paint.Cap.ROUND);
+        alpha = 0xff;
+        isVarRadius = false;
+    }
+
+    private void initFluffy() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.MITER);
+        paint.setStrokeCap(Paint.Cap.SQUARE);
+        paint.setStrokeMiter(0.2f);
+        paint.setPathEffect(new DiscretePathEffect(2, 2));
+        alpha = 0x20;
+        isVarRadius = false;
     }
 
     @Override
@@ -77,7 +107,9 @@ public class SketchView extends View {
     }
 
     private void initBitmap() {
-        bitmap = Bitmap.createBitmap(getWidth() / scaleFactor, getHeight() / scaleFactor,
+        bitmap = Bitmap.createBitmap(
+                (int) (getWidth() / scaleFactor),
+                (int) (getHeight() / scaleFactor),
                 Bitmap.Config.ARGB_8888);
         canvas = new Canvas(bitmap);
         canvas.drawColor(Color.WHITE);
@@ -96,17 +128,25 @@ public class SketchView extends View {
         float eventY = event.getY() / scaleFactor;
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: {
-                trackLength = 0;
+                prevPoint = null;
+                radius(baseRadius / scaleFactor);
                 backup.add(bitmap);
                 cloneBitmap();
                 path.moveTo(eventX, eventY);
                 break;
             }
             case MotionEvent.ACTION_UP: {
+                if(path.isEmpty()) {
+                    path.moveTo(prevX, prevY);
+                }
+                path.lineTo(eventX+1, eventY);
+                canvas.drawPath(path, paint);
+                path.reset();
+                invalidate();
+
                 prevX = 0;
                 prevY = 0;
                 prevPoint = null;
-                path.reset();
                 break;
             }
             case MotionEvent.ACTION_MOVE: {
@@ -115,20 +155,34 @@ public class SketchView extends View {
                 }
                 path.lineTo(eventX, eventY);
 
-                /*int radius = 100 / scaleFactor - trackLength;
-                if(radius < scaleFactor * 10) {
-                    radius = scaleFactor * 10;
+                if (isVarRadius) {
+                    float absLength = (baseRadius / scaleFactor) - (Math.abs(eventX - prevX) + Math.abs(eventY - prevY));
+                    float r = getRadius();
+                    if (r < absLength) {
+                        r += 1;
+                    } else {
+                        r -= 1;
+                    }
+                    if (r > 10 && r < (baseRadius / scaleFactor) && prevPoint != null) {
+                        radius(r);
+                    }
                 }
-                radius(radius);*/
 
                 canvas.drawPath(path, paint);
                 path.reset();
 
                 prevX = eventX;
                 prevY = eventY;
-                trackLength ++;
+
+                if (prevPoint == null) {
+                    prevPoint = new Point(eventX, eventY);
+                } else {
+                    prevPoint.setX(eventX);
+                    prevPoint.setY(eventY);
+                }
 
                 invalidate();
+
                 break;
             }
         }
@@ -159,14 +213,14 @@ public class SketchView extends View {
 
     public void color(int color) {
         paint.setColor(0xffffffff);
-        paint.setColor(Color.argb(0x50, Color.red(color), Color.green(color), Color.blue(color)));
+        paint.setColor(Color.argb(alpha, Color.red(color), Color.green(color), Color.blue(color)));
     }
 
-    public void radius(int radius) {
+    public void radius(float radius) {
         paint.setStrokeWidth(radius);
     }
 
-    public int getRadius() {
-        return (int) paint.getStrokeWidth();
+    public float getRadius() {
+        return paint.getStrokeWidth();
     }
 }
