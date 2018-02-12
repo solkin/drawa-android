30eb0b2c7646a07340056d59dc86396a00a1e0f7
Igor Solkin <inbox@tomclaw.com>
2018-02-12 21:20:39 +0300

    Add base draw components implementation

--- app/src/main/java/com/tomclaw/drawa/draw/DrawPresenter.kt
+++ app/src/main/java/com/tomclaw/drawa/draw/DrawPresenter.kt
@@ -2,6 +2,7 @@ package com.tomclaw.drawa.draw
 
 import android.os.Bundle
 import com.tomclaw.drawa.util.SchedulersFactory
+import io.reactivex.disposables.CompositeDisposable
 
 interface DrawPresenter {
 
@@ -27,18 +28,29 @@ class DrawPresenterImpl(private val interactor: DrawInteractor,
                         private val schedulers: SchedulersFactory,
                         state: Bundle?) : DrawPresenter {
 
+    private var view: DrawView? = null
+    private var router: DrawPresenter.DrawRouter? = null
+
+    private val subscriptions = CompositeDisposable()
+
     override fun attachView(view: DrawView) {
+        this.view = view
     }
 
     override fun detachView() {
+        subscriptions.clear()
+        this.view = null
     }
 
     override fun attachRouter(router: DrawPresenter.DrawRouter) {
+        this.router = router
     }
 
     override fun detachRouter() {
+        this.router = null
     }
 
-    override fun saveState() = Bundle.EMPTY
+    override fun saveState() = Bundle().apply {
+    }
 
 }
--- app/src/main/java/com/tomclaw/drawa/draw/DrawView.kt
+++ app/src/main/java/com/tomclaw/drawa/draw/DrawView.kt
@@ -1,6 +1,8 @@
 package com.tomclaw.drawa.draw
 
+import android.support.v7.widget.Toolbar
 import android.view.View
+import com.tomclaw.drawa.R
 
 interface DrawView {
 
@@ -8,4 +10,10 @@ interface DrawView {
 
 class DrawViewImpl(view: View) : DrawView {
 
+    private val context = view.context
+    private val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+
+    init {
+        toolbar.setTitle(R.string.draw)
+    }
 }
