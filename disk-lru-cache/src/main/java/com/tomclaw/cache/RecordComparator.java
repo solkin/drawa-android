a508989833322f108ffca040d7e0720d5acdb159
Igor Solkin <inbox@tomclaw.com>
2018-09-03 23:56:54 +0300

    Replace custom long type comparing with native

--- disk-lru-cache/src/main/java/com/tomclaw/cache/RecordComparator.java
+++ disk-lru-cache/src/main/java/com/tomclaw/cache/RecordComparator.java
@@ -10,6 +10,6 @@ class RecordComparator implements Comparator<Record> {
     }
 
     private static int compare(long x, long y) {
-        return (x < y) ? -1 : ((x == y) ? 0 : 1);
+        return Long.compare(x, y);
     }
 }
