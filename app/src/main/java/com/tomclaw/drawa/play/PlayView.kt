f70a612acec4ee7ae2e151510d061f8b7c1cd11d
Igor Solkin <inbox@tomclaw.com>
2019-06-28 22:39:21 +0300

    Add play view skeleton

--- app/src/main/java/com/tomclaw/drawa/play/PlayView.kt
+++ app/src/main/java/com/tomclaw/drawa/play/PlayView.kt
@@ -1,2 +1,28 @@
 package com.tomclaw.drawa.play
 
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import com.jakewharton.rxrelay2.PublishRelay
+import com.tomclaw.drawa.R
+import io.reactivex.Observable
+
+interface PlayView {
+
+    fun navigationClicks(): Observable<Unit>
+
+}
+
+class PlayViewImpl(view: View) : PlayView {
+
+    private val toolbar: Toolbar = view.findViewById(R.id.toolbar)
+
+    private val navigationRelay = PublishRelay.create<Unit>()
+
+    init {
+        toolbar.setTitle(R.string.info)
+        toolbar.setNavigationOnClickListener { navigationRelay.accept(Unit) }
+    }
+
+    override fun navigationClicks(): Observable<Unit> = navigationRelay
+
+}
